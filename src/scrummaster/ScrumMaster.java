6e844fd18019ae5bc9c3f213ccb6a1136cd7e66a
StormStake <59213963+StormStake@users.noreply.github.com>
2022-11-29 10:56:36 -0600

    test

---
+++ src/scrummaster/ScrumMaster.java
@@ -0,0 +1,17 @@
+
+package scrummaster;
+
+/**
+ *
+ * @author aaronjanssen
+ */
+public class ScrumMaster {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
