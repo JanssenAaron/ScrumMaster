af1bbf01661d244fccb60207bcbda8979ebef2d7
StormStake <59213963+StormStake@users.noreply.github.com>
2022-12-11 14:03:35 -0600

    emp

--- src/scrummaster/dataclasses/Employee.java
+++ src/scrummaster/dataclasses/Employee.java
@@ -1,8 +1,11 @@
 package scrummaster.dataclasses;
 
 import java.sql.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 import scrummaster.DBConnection;
+import scrummaster.enums.Request;
 
 public class Employee extends ScrumMasterCommand {
     // TODO Aaron
@@ -12,8 +15,10 @@ public class Employee extends ScrumMasterCommand {
     private String lName;
     private int roleAssignment;
     private int devTeamId;
+
     public Employee() {
     }
+
     public Employee(int id, String fName, String lName, int roleAssignment, int devTeamId) {
         this.id = id;
         this.fName = fName;
@@ -101,6 +106,87 @@ public class Employee extends ScrumMasterCommand {
             count++;
         }
         return count;
+    }
+
+    private ArrayList<Employee> getAllEmployees() {
+        try {
+            ResultSet emps = DBConnection.CONNECTION.createStatement().executeQuery("SELECT * FROM EMPLOYEE");
+            ArrayList<Employee> employees = new ArrayList<>();
+            while (emps.next()) {
+                int id = emps.getInt(1);
+                String fName = emps.getString(2);
+                String lName = emps.getString(3);
+                int ra = emps.getInt(4);
+                int devId = emps.getInt(5);
+                Employee newGuy = new Employee(id, fName, lName, ra, devId);
+                employees.add(newGuy);
+            }
+            return employees;
+
+        } catch (SQLException sqlE) {
+            System.out.println(sqlE.getMessage());
+            return null;
+        }
+
+    }
+
+    public void listFunction(Request req) {
+
+        ArrayList<Employee> emps = getAllEmployees();
+        for (Employee emp : emps) {
+            System.out.println(emp);
+        }
+
+    }
+
+    public void deleteFunction(Request req) {
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Employee ID to delete: ");
+            while (!scan.hasNextInt()) {
+                System.out.print("Employee ID to delete: ");
+                scan.next();
+            }
+            int id = scan.nextInt();
+            PreparedStatement psDelScrumM = DBConnection.CONNECTION
+                    .prepareStatement("DELETE FROM scrum_master WHERE scrum_master_id = ?");
+            PreparedStatement psDelEmpl = DBConnection.CONNECTION
+                    .prepareStatement("DELETE FROM employee WHERE employee_id = ?");
+            psDelScrumM.setInt(1, id);
+            psDelEmpl.setInt(1, id);
+            psDelScrumM.executeUpdate();
+            psDelEmpl.executeUpdate();
+            System.out.println("Deleted employee ID: " + Integer.toString(id));
+
+        } catch (SQLException sqlE) {
+            sqlE.printStackTrace();
+            System.out.println("Could not delete employee");
+        }
+    }
+
+    public void insertFunction(Request req) {
+        // int id = getInt();
+        String fName = getString();
+        String lName = getString();
+        int ra = getInt();
+        int devID = getInt();
+        try {
+
+            PreparedStatement ps = DBConnection.CONNECTION.prepareStatement(
+                    "INSERT INTO employee(first_name, last_name, role_assignment, dev_team_id) VALUES (?, ?, ?, ?)");
+            ps.setString(1, fName);
+            ps.setString(2, lName);
+            ps.setInt(3, ra);
+            ps.setInt(4, devID);
+            ps.executeUpdate();
+
+        } catch (SQLException sqlE) {
+            System.out.println(sqlE.getMessage());
+        }
+    }
+
+    public String toString() {
+        return "ID: " + getID() + " Name: " + getLastName() + ", " + getFirstName() + " Role: " + getRoleAssignment();
     }
 
 }
